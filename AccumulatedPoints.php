6aa5674cd20a46a758dfc8c604503271e193fbb0
jimlenart <jimlenart@gmail.com>
2016-02-29 18:09:09 -0600

    Update AccumulatedPoints.php

--- AccumulatedPoints.php
+++ AccumulatedPoints.php
@@ -1,8 +1,4 @@
 <?php
-include("Team.php"); //interesting... Need to include this class here...
-include("TeamResult.php");
-include("AccumulatedResults.php");
-include("Tools.php");
 
 /**
  * Created by PhpStorm.
@@ -18,76 +14,25 @@ include("Tools.php");
  * 3. Eventually have some nice UI
  */
 
-    $teamResults = new TeamResult(0,0,0);
-    $accumulatedResults = new AccumulatedResults(0,0,0,0);
-    $team = new Team('',0);
-    $scoreInfo = array();
-    $resultInfo = array();
-    $teams = array();
+class AccumulatedPoints
+{
+    public function __construct()
+    {
 
-    $scoreInfo = $teamResults->loadResultsFromDB();
-    $teams = $team->LoadTeamInfoFromDB();
-    $numberOfTeams = count($teams);
+        $teamResults = new TeamResult(0,0,0);
+        $accumulatedResults = new AccumulatedResults(0,0,0,0);
+        $team = new Team('',0);
 
-    $resultInfo = $accumulatedResults->calculateRank($scoreInfo, $numberOfTeams);
-    $resultInfo = $accumulatedResults->calculateAccumulatedResults($resultInfo);
+        $scoreInfo = $teamResults->GetTeamResultsFromDB();
+        $teams = $team->GetTeamsFromDBSortedByAccumulatedPoints();
+        $numberOfTeams = count($teams);
 
+        $resultInfo = $accumulatedResults->calculateRank($scoreInfo, $numberOfTeams); //should we just grab this info from DB instead of calculate it?
+        $resultInfo = $accumulatedResults->calculateAccumulatedResults($resultInfo);
+        $accumulatedResults->loadAccumulatedResultsIntoDB($resultInfo);
 
-    //so it seems like i should just cull the accumulated points table
-    //per team and store all results data for one team in a multi-dimensional array
+        buildAccumulatedPoints($resultInfo, $teams);
+    }
+}
+
 ?>
-
-<html>
-<head>
-    <link rel="stylesheet" type="text/css" href="AccumulatedPoints.css">
-    <title>Results Input Form</title>
-</head>
-<body>
-
-<div class="header">
-    <h1>Angry Robots Accumulated Points</h1>
-</div>
-
-<table>
-    <!-- If we find a new week add it to the column header-->
-    <thead>
-    <tr>
-        <th>Teams</th>
-        <?php
-            $currentWeek = 0;
-            for ($i=0; $i<count($resultInfo); ++$i)
-            {
-                if($currentWeek != $scoreInfo[$i]['week'])
-                {
-                    echo '<th>Week ' . $scoreInfo[$i]['week'] . '</th>';
-                }
-                $currentWeek = $scoreInfo[$i]['week'];
-            }
-        ?>
-    </tr>
-    </thead>
-
-    <tbody>
-    <?php
-        $currentTeamId = 0;
-        for ($i=0; $i<count($teams); ++$i)
-        {
-            echo '<tr>';
-            echo '<td>' . $teams[$i]['teamName'] . '</td>';
-            for ($j=0; $j<count($scoreInfo); ++$j)
-            {
-                if($teams[$i]['teamId'] == $scoreInfo[$j]['teamId'])
-                {
-                    echo '<td>' . $resultInfo[$j]['weeklyRank'] . '</td>';
-                }
-            }
-            echo '</tr>';
-        }
-    ?>
-
-    </tbody>
-</table>
-
-</body>
-</html>
-
