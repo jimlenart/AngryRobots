b27938915e4709a46dfde320f08b17e63f81c9c5
jimlenart <jimlenart@gmail.com>
2016-03-03 16:53:29 -0600

    Delete create_table_accumulated_points.sql

--- create_table_accumulated_points.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `team_results` (
-  `year` smallint(6) NOT NULL,
-  `week` smallint(6) NOT NULL,
-  `team_id` smallint(6) NOT NULL,
-  `score` float DEFAULT NULL,
-  `rank` float DEFAULT NULL,
-  PRIMARY KEY (`year`,`week`,`team_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
