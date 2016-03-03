d8507c8c350e9c16815c5e6d24ad0b4bb6d41889
jimlenart <jimlenart@gmail.com>
2016-03-03 16:53:47 -0600

    Added files via upload

---
+++ database/create_table_seasons.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `angry_robots`.`seasons` (
+  `year` INT NOT NULL,
+  `weeks` INT NOT NULL,
+  `current_season` VARCHAR(1) NULL,
+  PRIMARY KEY (`year`));
---
+++ database/create_table_teams.sql
@@ -0,0 +1,4 @@
+CREATE TABLE `teams` (
+  `team_id` smallint(6) DEFAULT NULL,
+  `team_name` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
